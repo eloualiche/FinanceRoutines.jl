aaf3cece0251e419b2b6e483b63f457951919872
Erik Loualiche <eloualic@umn.edu>
2025-01-08 15:39:57 -0600

    add local test running

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,16 +9,49 @@ import LibPQ: Connection
 
 
 # --------------------------------------------------------------------------------------------------
-@testset "FinanceRoutines.jl" begin
+function printtestset(desc)
+    # Get the current test set stack TODO
+    ts = Test.get_testset_depth()
+    # Calculate indent based on depth(fixed for now)
+    indent = "  " ^ (length(ts))
+    println(indent, "→ Running: ", desc)
+end
+# --------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------
+# To run file locally (where environment variables are not defined by CI)
+env_file = "../.env.gpg"
+if isfile(env_file)
+    io = IOBuffer(); run(pipeline(`which gpg`; stdout=io)); gpg_cmd = strip(String(take!(io)))
+    io = IOBuffer();
+    cmd = run(pipeline(`$gpg_cmd --decrypt $env_file`; stdout=io, stderr=devnull));
+    env_WRDS = String(take!(io))
+    # populate the environment variables
+    for line in split(env_WRDS, "\n")
+        !startswith(line, "#") || continue
+        isempty(strip(line)) && continue
+        if contains(line, "=")
+            key, value = split(line, "=", limit=2)
+            ENV[strip(key)] = strip(value)
+        end
+    end
+end
+# --------------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------------
+@testset verbose=true "FinanceRoutines.jl" begin
     # Write your tests here.
 
-    @testset "For debugging ..." begin
+    @testset "Debugging tests ..." begin
     # FOR DEBUGGING
         @test FinanceRoutines.greet_FinanceRoutines() == "Hello FinanceRoutines!"
         @test FinanceRoutines.greet_FinanceRoutines() != "Hello world!"
     end
 
     @testset "Importing Fama-French factors from Ken French library" begin
+        printtestset("Ken French")
         # import_FF3
         df_FF3 = FinanceRoutines.import_FF3();
         @test names(df_FF3) == ["datem", "mktrf", "smb", "hml",  "rf"];
@@ -35,14 +68,16 @@ import LibPQ: Connection
         @test !isempty(WRDS_PWD)
     end
 
-    @testset "WRDS tests ... downloads and build" begin
-        WRDS_USERNAME = get(ENV, "WRDS_USERNAME", "")
-        WRDS_PWD = get(ENV, "WRDS_PWD", "")
+    @testset verbose=true "WRDS tests ... downloads and build" begin
+        printtestset("WRDS")
+        WRDS_USERNAME = get(ENV, "WRDS_USERNAME", "");
+        WRDS_PWD = get(ENV, "WRDS_PWD", "");
 
         wrds_conn = FinanceRoutines.open_wrds_pg(WRDS_USERNAME, WRDS_PWD)
         @test typeof(wrds_conn) == Connection
 
         @testset "CRSP MSF" begin
+            printtestset("CRSP")
             df_msf = import_MSF(wrds_conn; date_range = (Date("2000-01-01"), Date("2002-01-01")));
             build_MSF!(df_msf; clean_cols=true);
 
@@ -52,6 +87,7 @@ import LibPQ: Connection
         end
 
         @testset "Compustat FUNDA" begin
+            printtestset("Compustat")
             df_funda = import_Funda(wrds_conn;
                 date_range = (Date("2000-01-01"), Date("2002-01-01")),
                 variables=["PPENT", "NAICSH", "NAICS"])
